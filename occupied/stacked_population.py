49fdd68e84c021fc93338dbe66f2a0c773ca3108
Aaron <aaronchoo9@gmail.com>
2020-05-23 18:37:03 -0400

    moved files to subdirectory

--- occupied/stacked_population.py
+++ occupied/stacked_population.py
@@ -14,33 +14,33 @@ def PreparePlotData(dataPath):
     
     # Sort the dates to be in proper order
     days = sorted(data.OCCUPANCY_DATE.unique())
+    return [occupied[day] for day in days], [capacity[day] - occupied[day] for day in days], [day[4:10] for day in days]
 
-    return [occupied[day] for day in days], [capacity[day] - occupied[day] for day in days], [day[:10] for day in days]
+def CreatedStackPlot(occupied, capacity, labels, year):
+    # Axis data and removes the year from label
+    x = [label[5:] for label in labels] 
+    y = [occupiedCreatedStackPlot, capacity]
 
-
-
-def CreatedStackPlot(occupied, capacity, labels):
-    # Axis data
-    x = labels
-    y = [occupied, capacity]
-    # use a known color palette (see..)
+    # Set up graph data
     pal = sns.color_palette("Set1")
     plt.stackplot(x,y, labels=['Occupied', 'Avaliable'], colors=pal, alpha=0.7)
     plt.legend(loc='lower right')
     plt.margins(0,0)
     plt.title('Shelter Occupied vs Avaliable')
-    # plt.show()
-    plt.savefig("Shelter-{}.png".format(labels[0][:4]))
+
+    # Save the graph (can use plt.show() to display graph instead)
+    plt.savefig("Shelter-{}.pdf".format(year))
+    
+    # Clear all configurations
     plt.clf()
 
 
-def GenerateAllPlots():
-    paths = glob.glob("data/*.csv")
+def GenerateStackPlots():
+    paths = glob.glob("../data/shelter-*.csv")
     for path in paths:
-        print(path)
         occupied, capacity, labels = PreparePlotData(path)
         if len(occupied):
-            CreatedStackPlot(occupied, capacity, labels)
-    #print(occupied, capacity, len)
+            CreatedStackPlot(occupied, capacity, labels, path[-8:-4])
     
-GenerateAllPlots()
+if __name__ == "__main__":
+   GenerateStackPlots()
