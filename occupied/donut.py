1b385fc0fb4994577b0d330187b2eafe628bbc56
Aaron <aaronchoo9@gmail.com>
2020-06-07 18:50:01 -0400

    generated occupied and capacity plots

--- occupied/donut.py
+++ occupied/donut.py
@@ -4,16 +4,23 @@ import pandas as pd
 import glob
 
 
-def prepare_donut_ingredients(data_path, column=None):
+def prepare_donut_ingredients(data_path):
     data = pd.read_csv(data_path, index_col = 0).fillna(0)
     # contains the sector's name as key and count
-    sectors = {}
+    sectors_shelter_count, sectors_occupancy, sectors_capacity = {}, {}, {}
     
     # Group sectors names
     for index, row in data.iterrows():
-        sectors[row.SECTOR] = sectors.get(row.SECTOR, 0) + row[column] if column is not None else 1
+        sectors_shelter_count[row.SECTOR] = sectors_shelter_count.get(row.SECTOR, 0) + 1
+        sectors_occupancy[row.SECTOR] = sectors_occupancy.get(row.SECTOR, 0) + row["OCCUPANCY"]
+        sectors_capacity[row.SECTOR] = sectors_capacity.get(row.SECTOR, 0) + row["CAPACITY"]
 
-    return list(sectors.keys()), list(sectors.values())
+    sector_types = list(sectors_shelter_count.keys())
+    sector_shetler_count = list(sectors_shelter_count.values())
+    sector_occupancy_sum = list(sectors_occupancy.values())
+    sector_capacity_sum = list(sectors_capacity.values())
+
+    return sector_types, sector_shetler_count, sector_occupancy_sum, sector_capacity_sum
 
 
 def create_donut(types, weighting, weighted_on, year):
@@ -27,7 +34,7 @@ def create_donut(types, weighting, weighted_on, year):
     plt.pie(size, labels=names, colors=Pastel1_7.hex_colors)
     p=plt.gcf()
     p.gca().add_artist(my_circle)
-    plt.title("Sectors based on {} counted in {}".format(weighted_on, year))
+    plt.title("Daily shelter sectors based on total {} counted in {}".format(weighted_on, year))
     # Save the donut (can use plt.show() to display graph instead)
     plt.savefig("../plots/Sectors-donut-{}-{}.pdf".format(weighted_on, year))
     
@@ -38,12 +45,13 @@ def create_donut(types, weighting, weighted_on, year):
 def donut_make_plots():
     paths = glob.glob("../data/shelter-*.csv")
     for path in paths:
-        types, weighting = prepare_donut_ingredients(path)
-        if len(types):
-            create_donut(types, weighting, "shelters", path[-8:-4])
-        types, weighting = prepare_donut_ingredients(path, "CAPACITY")
-        if len(types):
-            create_donut(types, weighting, "capacity", path[-8:-4])
+        types, shelter_count, sum_occupancy, sum_capacity = prepare_donut_ingredients(path)
+        if len(shelter_count):
+            create_donut(types, shelter_count, "shelters", path[-8:-4])
+        if len(sum_occupancy):
+            create_donut(types, sum_occupancy, "occupancy", path[-8:-4])
+        if len(sum_capacity):
+            create_donut(types, sum_capacity, "capacity", path[-8:-4])
 
 
 if __name__ == "__main__":
